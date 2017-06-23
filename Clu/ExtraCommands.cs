4e06355b9a9216b330fecf0c7d5a5b3ea8b17cd7
Jay Malhotra <jay0@me.com>
2017-06-24 00:22:20 +0100

    Changed title URL to redirect to my CSE
    It no longer redirects to the main Google page as I found that results are seldom reproducible between the two - likely because the main Google page is laden with algorithms analyzing your past usage and cookies, as well as
    your location, and all that sort of stuff. Especially with the removal of the ability to specify more than three results (well, it wasn't really an ability as it never worked, but I digress) it is especially important to get reproducibility right.

--- Clu/ExtraCommands.cs
+++ Clu/ExtraCommands.cs
@@ -129,7 +129,10 @@ namespace Clu
                     .WithText($"Search completed in {SearchTimeTaken.Milliseconds}ms."))
                 // Set the URL that our title will point to as the actual Google results page
                 // for the given query, should the user wish to know more.
-                .WithUrl($"http://www.google.com/search?q={Query.Replace(" ", "%20")}");
+                // Points to my CSE so as to ensure results are (for the most part) the same.
+                .WithUrl("https://cse.google.co.uk/cse/" +
+                        "publicurl?cx=011947631902407852034:gq02yx0e1mq" +
+                        $"&q={Query.Replace(" ", "%20")}");
                     
             foreach (Result r in Results) {
                 Embed.AddField(
