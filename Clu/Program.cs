3bef91e2743632efec68c2c7f3f95b6f12487f05
Jay Malhotra <jay0@me.com>
2017-06-23 19:13:20 +0100

    Add dedicated Keychain class, extend logging functionality
    For both changes, the idea is to make keys and logging more easily accessible outside of the Program class. As I understand it, the Log method is somewhat critical to discord.NET's operations, so I've merely extended it by adding my own version in there, which is static and can be used outside. I also moved the colors and prefixes there; as they are static the main Program class can draw from them still just fine, but they need to be in there to maintain full logging coverage for other classes.

--- Clu/Program.cs
+++ Clu/Program.cs
@@ -36,7 +36,11 @@ namespace Clu
         public async Task MainAsync()
         {
             StartTime = DateTime.UtcNow;
-            string Token = RetrieveToken();
+
+            // Get Discord bot token
+            Keychain.DiscordBotToken = Keychain.RetrieveFromKeychain("token.txt");
+            // Get Google API key
+            Keychain.GoogleAPIKey = Keychain.RetrieveFromKeychain("searchkey.txt");
             
             Client = new DiscordSocketClient();
             Client.Log += Log;
@@ -46,7 +50,7 @@ namespace Clu
             Commands = new CommandService();
             await InitCommands();
 
-            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.LoginAsync(TokenType.Bot, Keychain.DiscordBotToken);
             await Client.StartAsync();
 
             // Prevent the program exiting
@@ -71,36 +75,14 @@ namespace Clu
             var result = await Commands.ExecuteAsync(Context, ArgStart, Services);
 
             if (!result.IsSuccess)
-                await this.Log(new LogMessage(LogSeverity.Warning, "HandleCommand", 
+                await Log(new LogMessage(LogSeverity.Warning, "HandleCommand", 
                 $"Encountered an error while trying to parse potential command {Message.Content}: {result.ErrorReason} ({result.Error})"));
         }
-
-        // *** Logging
-        public Dictionary<LogSeverity, ConsoleColor> LogColors = new Dictionary<LogSeverity, ConsoleColor>() 
-        {
-            { LogSeverity.Critical, ConsoleColor.DarkRed },
-            { LogSeverity.Error, ConsoleColor.Red},
-            { LogSeverity.Debug, ConsoleColor.Magenta },
-            { LogSeverity.Info, ConsoleColor.Cyan },
-            { LogSeverity.Warning, ConsoleColor.Yellow },
-            { LogSeverity.Verbose, ConsoleColor.Gray },
-        };
-
-        public Dictionary<LogSeverity, string> LogPrefixes = new Dictionary<LogSeverity, string>()
-        {
-            { LogSeverity.Critical, "CRITICAL"},
-            { LogSeverity.Error, "ERROR"}, // Hopefully having those two in caps will make them easier to spot
-            { LogSeverity.Debug, "Debug"},
-            { LogSeverity.Info, "Info"},
-            { LogSeverity.Warning, "Warn"},
-            { LogSeverity.Verbose, "Info++"},
-        };
-
-
+        
         private Task Log(LogMessage Message)
         {
-            Console.ForegroundColor = LogColors[Message.Severity];
-            Console.Write($"[{LogPrefixes[Message.Severity]}][{Message.Source}] {Message.ToString()}");
+            Console.ForegroundColor = AuxillaryLogger.LogColors[Message.Severity];
+            Console.Write($"[{AuxillaryLogger.LogPrefixes[Message.Severity]}][{Message.Source}] {Message.ToString()}");
             if (Message.Exception != null)
                 Console.Write($" ({Message.Exception.ToString()}: {Message.Exception.Message})");
             
@@ -114,31 +96,109 @@ namespace Clu
 
         // Some filepath auto-properties. These are here because the System.IO function calls aren't exactly concise,
         // but are used a lot in making things universal and relative. Saves us some typing
-        public string BaseFilepath
+        public static string BaseFilepath
         {
             get { return AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.IndexOf("bin")); }
             // Get the app directory and trim it up to before bin/ so we get the root dir which this file is in
         }
+    }
 
-        public string KeychainPath
+    static class Keychain
+    {
+        public static string DiscordBotToken { get; set; }
+        public static string GoogleAPIKey { get; set; }
+
+        public static string KeychainPath
         {
-            get { return System.IO.Path.Combine(BaseFilepath, "keychain"); }
+            get { return System.IO.Path.Combine(Program.BaseFilepath, "keychain"); }
             // Add on the Keychain folder
         }
 
-        public string FileInKeychain(string filename)
+        public static string FileInKeychain(string Filename)
         {
-            return System.IO.Path.Combine(KeychainPath, filename);
+            return System.IO.Path.Combine(KeychainPath, Filename);
         }
 
-        private string RetrieveToken()
+        public static string RetrieveFromKeychain(string Filename)
         {
-            string RawToken = System.IO.File.ReadAllLines(FileInKeychain("token.txt"))[0];
-            // Clean token
-            string Token = RawToken.Replace("\n", "");
+            // Get the first line so we miss any formatting
+            try {
+                string RawKey = System.IO.File.ReadAllLines(FileInKeychain(Filename))[0];
+                // Clean token
+                string Key = RawKey.Replace("\n", "");
+
+                return Key;
+            } catch(System.IO.FileNotFoundException) { 
+                if (Filename == "token.txt") {
+                    // Sorry, but the script is totally useless without a token
+                    AuxillaryLogger.Log(new LogMessage(LogSeverity.Critical, "Login", 
+                    "Failed to retrieve bot token from token.txt in keychain folder. " +
+                    "The script must now exit. Refer to the README to see how to set up " +
+                    "a bot and get its user account token."));
+                    Utils.Exit();
+                } else {
+                    // The other API keys are somewhat optional
+                    AuxillaryLogger.Log(new LogMessage(LogSeverity.Warning, "Keychain retrieval",
+                    $"Failed to retrieve token/API key '{Filename}'. " +
+                    "Some functions of the bot may be unavailable as a result."));
+                }
+                return null; 
+            } catch(Exception e) {
+                // If don't know what happened, then let the user know.
+                // Some exceptions which aren't covered here should be obvious to any user
+                // e.g. UnauthorizedAccessException/SecurityException
+                AuxillaryLogger.Log(new LogMessage(LogSeverity.Error, "Keychain retrieval",
+                $"In attempting to retrieve data from the keychain, an unknown error occured.\n" +
+                $"Error details are as follows: \"{e.ToString()} - {e.Message}\""));
+                return null;
+            }
+        }
+    }
+
+    // A logger for me to use without messing with the one required by the bot.
+    // (in keeping with the Open Closed principle if you want to spin it like that)
+    static class AuxillaryLogger
+    {
+        // *** Logging
+        public static Dictionary<LogSeverity, ConsoleColor> LogColors = new Dictionary<LogSeverity, ConsoleColor>() 
+        {
+            { LogSeverity.Critical, ConsoleColor.DarkRed },
+            { LogSeverity.Error, ConsoleColor.Red},
+            { LogSeverity.Debug, ConsoleColor.Magenta },
+            { LogSeverity.Info, ConsoleColor.Cyan },
+            { LogSeverity.Warning, ConsoleColor.Yellow },
+            { LogSeverity.Verbose, ConsoleColor.Gray },
+        };
+
+        public static Dictionary<LogSeverity, string> LogPrefixes = new Dictionary<LogSeverity, string>()
+        {
+            { LogSeverity.Critical, "CRITICAL"},
+            { LogSeverity.Error, "ERROR"}, // Hopefully having those two in caps will make them easier to spot
+            { LogSeverity.Debug, "Debug"},
+            { LogSeverity.Info, "Info"},
+            { LogSeverity.Warning, "Warn"},
+            { LogSeverity.Verbose, "Info++"},
+        };
+        public static void Log(LogMessage Message)
+        {
+            Console.ForegroundColor = LogColors[Message.Severity];
+            Console.Write($"[{LogPrefixes[Message.Severity]}][{Message.Source}] {Message.ToString()}");
+            if (Message.Exception != null)
+                Console.Write($" ({Message.Exception.ToString()}: {Message.Exception.Message})");
             
-            Log(new LogMessage(LogSeverity.Info, "Core", $"Got token: \"{Token}\""));
-            return Token;
+            Console.Write("\n");
+            // Output e.g. [ERROR][source] Something went wrong
+            Console.ResetColor();
+        }
+    }
+
+    static class Utils
+    {
+        public static void Exit()
+        {
+            Console.WriteLine("Press Enter to exit...");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) {}
+            Environment.Exit(0);
         }
     }
 }
