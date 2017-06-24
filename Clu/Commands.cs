20f58f4571c35c4192e4c96fa86d54e59ff8212e
SapiensAnatis <jay0@me.com>
2017-06-25 00:38:43 +0100

    Add months, years, decades to ?uptime
    Yes I was really bored. Months could actually be useful, as could weeks. If the bot is stable, then it being up for 3 or 4 months doesn't seem to farfetched. In which case, 120 days isn't as helpful wrt putting it in context

--- Clu/Commands.cs
+++ Clu/Commands.cs
@@ -23,15 +23,41 @@ public class CoreCommandModule : ModuleBase
         // In future I might spice it up a bit by including an embed (info card, basically) that also details recent
         // uptime percentages/other statistics.
         string UptimeString = String.Empty;
-        if (Uptime.TotalSeconds < 1) { UptimeString = "not very long at all"; }
-        else { UptimeString = $"{Uptime.Days} days, {Uptime.Hours} hours, {Uptime.Minutes} minutes and {Uptime.Seconds} seconds"; }
-        // No weeks method :(
 
+        // Extra timespan things
+        int TotalDecades = (int)(Uptime.TotalDays/3652.5);
+        int TotalYears = (int)(Uptime.TotalDays/365.25); // Leap years mann
+        int TotalMonths = (int)(Uptime.TotalDays/30.44); // Bit crap
+        int TotalWeeks = (int)(Uptime.TotalDays/7); // Always truncate
+        // We can only go back to 1970 in theory so decades is all we need
+        // The years and decades thing is a joke btw
+        // Now get to actual years/decades/etc
+        int Decades = TotalDecades;
+        int Years = TotalYears - Decades*10;
+        int Months = TotalMonths - TotalYears*12;
+        // According to Google
+        int Weeks = TotalWeeks - (int)(TotalDecades*4.34524);
+
+        if (Uptime.TotalSeconds < 1) { UptimeString = "not very long at all"; }
+        else { UptimeString = $"{Decades} decades, " +
+                              $"{Years} years, " +
+                              $"{Months} months, " +
+                              $"{Weeks} weeks, " +
+                              $"{Uptime.Days - TotalWeeks*7} days, " +
+                              $"{Uptime.Hours} hours, " +
+                              $"{Uptime.Minutes} minutes " +
+                              $"and {Uptime.Seconds} seconds"; }
+
+        RemoveSubstring(ref UptimeString, "0 decades, ");
+        RemoveSubstring(ref UptimeString, "0 years, ");
+        RemoveSubstring(ref UptimeString, "0 months, ");
+        RemoveSubstring(ref UptimeString, "0 weeks, ");
         RemoveSubstring(ref UptimeString, "0 days, ");
         RemoveSubstring(ref UptimeString, "0 hours, ");
         RemoveSubstring(ref UptimeString, "0 minutes and "); // Remove redundant time periods
 
         await ReplyAsync($"I've been running for {UptimeString}.");
+        
 
         
     }
