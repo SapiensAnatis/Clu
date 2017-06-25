9402a86b4a5b0bb2a66fc7a52ec0208c9c60fdfd
SapiensAnatis <jay0@me.com>
2017-06-25 01:39:30 +0100

    Plural fixes in timespam format
    Also fixed some other bugs relating to edge cases

--- Clu/Commands.cs
+++ Clu/Commands.cs
@@ -4,10 +4,17 @@ using Discord.WebSocket;
 
 using System;
 using System.Threading.Tasks;
-
+using System.Text.RegularExpressions;
 
 public class CoreCommandModule : ModuleBase
 {
+    public static Regex RemoveBadPlurals = new Regex(@"(?<!\d)1 \w*?(?=s)"); 
+    // Remove plurals from time expressions where the preceding quantity is equal to one 
+    // Matches on '1 days/1 minutes', etc, but only actually matches the '1 day' part so what
+    // the substitution is will be immediately obvious. Thanks lookaheads!  
+    // The beginning lookbehind is to ensure the tail end of 11 days or 31 days doesn't get caught
+    public static Regex NoDoubleSpace = new Regex(@"\s{2,}");
+
     [Command("uptime"), Summary("Find out how long the bot's current session has been running for")]
     public async Task GetUptime()
     {
@@ -16,9 +23,6 @@ public class CoreCommandModule : ModuleBase
                 Original = Original.Replace(Substring, "");
             }
         } // Internal function to strip 0hrs, 0days, etc.
-
-        // It's internal because I have no need for it elsewhere. Only accessible from this
-        // func. New in C# 7.0 :)
         
         TimeSpan Uptime = Clu.Program.Uptime;
         // Bit of spaghetti here. Could be better, but it's a simple command so not much point making it very fancy
@@ -31,16 +35,27 @@ public class CoreCommandModule : ModuleBase
         int TotalYears = (int)(Uptime.TotalDays/365.25); // Leap years mann
         int TotalMonths = (int)(Uptime.TotalDays/30.44); // Bit crap
         int TotalWeeks = (int)(Uptime.TotalDays/7); // Always truncate
-        // We can only go back to 1970 in theory so decades is all we need
         // The years and decades thing is a joke btw
         // Now get to actual years/decades/etc
         int Decades = TotalDecades;
         int Years = TotalYears - Decades*10;
         int Months = TotalMonths - TotalYears*12;
         // According to Google
-        int Weeks = TotalWeeks - (int)(TotalDecades*4.34524);
+        int Weeks = TotalWeeks - (int)(TotalYears*52);
 
-        if (Uptime.TotalSeconds < 1) { UptimeString = "not very long at all"; }
+        // It should be noted that this all breaks down with very large timespan values due to the natural innacuracy in
+        // for example stating that there are 4.34524 weeks per month. Because the values are nonsensical with some
+        // months having more days than others, we approximate.
+        // When using the DateTime.MinValue as the startup time, this happens: 
+        // "I've been running for 201 decades, 6 years, 3 months, 81 weeks, 5 days, 23 hours, 35 minutes and 1 seconds."
+        // Sadly, weeks is most affected because it's the smallest time period without an official method, and I'm bad at this
+        // If I use 52 weeks per year I get 382 weeks
+        //
+        // On the bright side, this is totally immaterial unless you're spoofing the times - uptimes should never ever reach
+        // over a year...I will therefore keep 52 weeks a year as it's more readable
+
+        if (Uptime.TotalSeconds < 1) { UptimeString = "not very long at all"; } 
+        // To avoid printing an empty string, in case a particularly fast-fingered user manages this
         else { UptimeString = $"{Decades} decades, " +
                               $"{Years} years, " +
                               $"{Months} months, " +
@@ -50,17 +65,32 @@ public class CoreCommandModule : ModuleBase
                               $"{Uptime.Minutes} minutes " +
                               $"and {Uptime.Seconds} seconds"; }
 
-        RemoveSubstrings(ref UptimeString,
-                        "0 decades", "0 years", "0 months", "0 weeks", 
-                        "0 days,", "0 hours", "0 minutes and");
-
         /* Okay, look, I know this code is really, really ugly. However, I was looking at replacing various
            string concatenations with StringBuilders, and many people say that the concat approach is faster
            if, and only if, the number of strings is known. In every case I've done so far (there's similar
            code in ExtraCommands.cs for making an API request) there have been a known number of concatenations.
            
            This is something to do with the fact that it gets optimized into a Concat call at runtime, which I think
-           is uglier than the plus ops, so I won't do it myself, but is much faster than a StringBuilder. */
+           is uglier than the plus ops, so I won't do it myself. Either way, the code is faster than a builder. */
+        
+        RemoveSubstrings(ref UptimeString,
+                        "0 decades,", "0 years,", "0 months,", "0 weeks,", 
+                        "0 days,", "0 hours,", "0 minutes ");
+        
+        int i = 0; // Index change tracker
+        foreach (Match m in RemoveBadPlurals.Matches(UptimeString))
+        {
+            string BadPlural = UptimeString.Substring(m.Index-i, m.Length+1);
+            UptimeString = UptimeString.Replace(BadPlural, m.Value);
+            // It actually matches the correct version of the plural using lookaheads.
+            // So the hardest part is finding the original part it took issue with
+            i++; // For every s removed, the string shifts left.
+        }
+
+        // Also just generally clean up
+        UptimeString = NoDoubleSpace.Replace(UptimeString, " ");
+        if (UptimeString[0] == ' ') // Remove leading spaces
+            UptimeString = UptimeString.Remove(0, 1);
            
         await ReplyAsync($"I've been running for {UptimeString}.");
 
