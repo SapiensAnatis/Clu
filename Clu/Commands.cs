4d76eed8380a80fef0a6658911f0913f9caf65fb
Jay Malhotra <jay0@me.com>
2017-06-27 19:58:17 +0100

    Add Commands.cs to the main namespace
    I'm used to VS generating the templates for me, so I failed to notice this was not in the right namespace. Somehow it still worked properly, I just found it strange I couldn't log from it.

--- Clu/Commands.cs
+++ Clu/Commands.cs
@@ -5,34 +5,36 @@ using Discord.WebSocket;
 using System;
 using System.Threading.Tasks;
 
-
-public class CoreCommandModule : ModuleBase
+namespace Clu 
 {
-    [Command("uptime"), Summary("Find out how long the bot's current session has been running for")]
-    public async Task GetUptime()
+    public class CoreCommandModule : ModuleBase
     {
-        void RemoveSubstring(ref string Original, string Substring) {
-            Original = Original.Replace(Substring, "");
-        } // Internal function to strip 0hrs, 0days, etc.
+        [Command("uptime"), Summary("Find out how long the bot's current session has been running for")]
+        public async Task GetUptime()
+        {
+            void RemoveSubstring(ref string Original, string Substring) {
+                Original = Original.Replace(Substring, "");
+            } // Internal function to strip 0hrs, 0days, etc.
 
-        // It's internal because I have no need for it elsewhere. Only accessible from this
-        // func. New in C# 7.0 :)
-        
-        TimeSpan Uptime = Clu.Program.Uptime;
-        // Bit of spaghetti here. Could be better, but it's a simple command so not much point making it very fancy
-        // In future I might spice it up a bit by including an embed (info card, basically) that also details recent
-        // uptime percentages/other statistics.
-        string UptimeString = String.Empty;
-        if (Uptime.TotalSeconds < 1) { UptimeString = "not very long at all"; }
-        else { UptimeString = $"{Uptime.Days} days, {Uptime.Hours} hours, {Uptime.Minutes} minutes and {Uptime.Seconds} seconds"; }
-        // No weeks method :(
+            // It's internal because I have no need for it elsewhere. Only accessible from this
+            // func. New in C# 7.0 :)
+            
+            TimeSpan Uptime = Clu.Program.Uptime;
+            // Bit of spaghetti here. Could be better, but it's a simple command so not much point making it very fancy
+            // In future I might spice it up a bit by including an embed (info card, basically) that also details recent
+            // uptime percentages/other statistics.
+            string UptimeString = String.Empty;
+            if (Uptime.TotalSeconds < 1) { UptimeString = "not very long at all"; }
+            else { UptimeString = $"{Uptime.Days} days, {Uptime.Hours} hours, {Uptime.Minutes} minutes and {Uptime.Seconds} seconds"; }
+            // No weeks method :(
 
-        RemoveSubstring(ref UptimeString, "0 days, ");
-        RemoveSubstring(ref UptimeString, "0 hours, ");
-        RemoveSubstring(ref UptimeString, "0 minutes and "); // Remove redundant time periods
+            RemoveSubstring(ref UptimeString, "0 days, ");
+            RemoveSubstring(ref UptimeString, "0 hours, ");
+            RemoveSubstring(ref UptimeString, "0 minutes and "); // Remove redundant time periods
 
-        await ReplyAsync($"I've been running for {UptimeString}.");
+            await ReplyAsync($"I've been running for {UptimeString}.");
 
-        
+            
+        }
     }
 }
